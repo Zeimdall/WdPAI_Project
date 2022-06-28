3eec3b12a7b9094ccae521c98bce2201fac05858
Piotr <piotrzych98@gmail.com>
2022-06-28 18:50:53 +0200

    add small changes

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+const USERNAME = 'qtotkfqgdftljl';
+const PASSWORD = '50b25d223ac792903e94dfe791055adcc40af195007d2267e969036ab6df2606';
+const HOST = 'ec2-52-73-184-24.compute-1.amazonaws.com';
+const DATABASE = 'd70rpm3fcprivg';
